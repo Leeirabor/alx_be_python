0564009ca08acf4478f2a559b2ff3aeddde9df0b
elisha <ieliviral@gmail.com>
2025-07-17 19:43:46 +0100

    match_case_calculator to git.

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter the First number:"))
+num2 = int(input  ("Enter the second number: "))
+operator = input ("Choose the operation (+, -, *, /):")
+
+match operator:
+    case "+":
+        print("The result is", num1 + num2)
+    case "-":
+        print("The result is", num1 - num2)   
+    case "*":
+        print("The result is", num1 * num2)
+    case "/":
+        if num2 == 0:
+             print("cannot devide by zero")
+        else:
+            print ("the result is",num1 / num2) 
+    case _:
+        print ("Invalid operator.")     
+
+
+
