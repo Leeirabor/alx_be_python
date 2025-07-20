f7bcc15b2d61ca17c7a9d97782a42666f985cb22
lee <ieliviral@gmail.com>
2025-07-20 01:44:56 +0100

    Uploading pattern_ drawing

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,3 +2,4 @@ num = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
+    
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,14 @@
+
+
+
+size = int(input("Enter the size of the pattern"))
+row = 0
+
+while  row < size:
+    for i in range (size):
+        print ("*",end="")
+        size + 1
+    print ()
+    row += 1
+
+    
