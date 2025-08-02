3c9d4f5b08b1359e252b6e3d6d8ba5cebeec84d6
Irabor <eirabor@xceneresearch.com>
2025-08-02 11:18:49 +0100

    added epriod to output in Robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,7 +6,7 @@ def safe_divide(numerator, denominator):
         result = num/ den
         return f"The result of the division is {result:.1f}"
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero"
+        return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: please enter numeric values only"
+        return "Error: Please enter numeric values only."
    
